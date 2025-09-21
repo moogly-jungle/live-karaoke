a80285fe3e62def388b3a12a20606b6a5ce7003c
Michel Billaud <michel.billaud@laposte.net>
2025-09-21 21:11:34 +0200

    nouvelle moulinette avec templates

---
+++ nouvelle-moulinette.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" 
+Generate song lists and individual song pages from text files.
+Requires jinja2 (pip install jinja2).
+"""
+
+import os
+import jinja2
+
+template_dir = 'templates'
+
+from songfilereader import SongFileReader    
+
+def get_songs_from_dir(dir_path):
+    """ Read all songs from a directory. Return a list of Song objects."""
+    song_reader = SongFileReader()
+    songs = []
+    for file in os.listdir(dir_path):
+        if file.endswith('.txt'):
+            song = song_reader.read_from(os.path.join(dir_path, file))
+            if not song is None:
+                songs.append(song)
+    return songs
+
+songs = get_songs_from_dir('paroles')
+
+# -------------------------------------------
+
+def csv_escape(text):
+    """ 
+    Filter for jinja2. Escape text for CSV output.
+    """
+    if '"' in text or ',' in text:
+        text = text.replace('"', '""')
+        return f'"{text}"'
+    return text
+
+jinja2.filters.FILTERS['csv_escape'] = csv_escape
+
+# tri des chansons par titre
+songs.sort(key=lambda s: s.header.get('title').lower())
+
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+lyrics_template = jinja_env.get_template('lyrics.html.j2')
+
+def generate_list(result_path, template_path, songs):
+    """ Generate a list file from a template and a list of songs."""
+    print(f'* generate list "{result_path}" from "{template_path}"')
+    template = jinja_env.get_template(template_path)
+    with open(result_path, 'w') as f:
+        f.write(template.render(songs=songs, lyrics_link_dir='lyrics', score_link_dir='partitions'))
+    # print('- list done')
+    
+generate_list('docs/chansons.txt', 'liste.txt.j2', songs)
+generate_list('docs/chansons.csv', 'simple.csv.j2', songs)
+generate_list("docs/public-list.html", 'public-list.html.j2', songs)
+
+
+for song in songs:
+    prefix = song.name.removesuffix('.txt')
+    output_path = f"docs/lyrics/{prefix}.html"
+    with open(output_path, 'w') as f:
+        # print(f'* generate lyrics "{output_path}"')
+        f.write(lyrics_template.render(song=song))
+print(f'- all {len(songs)} lyrics done')
+        
---
+++ songfilereader.py
@@ -0,0 +1,69 @@
+
+from dataclasses import dataclass
+import os
+
+
+class SongReaderError(Exception):
+    pass
+
+@dataclass
+class Song:
+    """ A song with its metadata (header) and lyrics."""
+    name: str        # filename, with .txt suffix 
+    header: dict[str]
+    lyrics: list[str]
+
+
+allowed_tags  = ['title', 'score', 'artist', 'youtube', 'tabs', 'lyrics']
+required_tags = ['title', 'artist', 'score' ]
+
+tag_aliases = {
+    'titre': 'title',
+    'artiste': 'artist',
+    'partition': 'score',
+    'paroles': 'lyrics'
+}
+
+class SongFileReader:  
+    
+    """ Read songs from files."""
+    
+    def read_from(self, file_path):
+        """ Read a song from a file. Return a Song object or None if error."""
+        with open(file_path, 'r') as f:
+            # get attributes from header
+            try:
+                header = self.get_header(f)   
+                lyrics = self.get_body(f) 
+                return Song(os.path.basename(file_path), header, lyrics)
+            except SongReaderError as e:
+                print(f'Error reading "{file_path}": {e}')
+        return None
+
+    def get_header(self, file):
+        """ Read the header part (metadata) of the song file."""
+        header = {}
+        line_no = 0
+        for line in file:
+            line_no += 1
+            if line.strip() == '':
+                break
+            try:
+                tag, value = [ x.strip() for x in line.split(':', 1)]
+                tag = tag_aliases.get(tag, tag)
+                if tag not in allowed_tags:
+                   raise SongReaderError(f'unknown tag "{tag}" line {line_no}\n---')
+                if tag == "lyrics":
+                   break
+                header[tag] = value
+            except ValueError:
+                raise SongReaderError(f'tag error line {line_no}\n---')
+        for tag in required_tags:
+            if tag not in header:
+                raise SongReaderError(f'missing required tag "{tag}"\n---')
+        return header
+ 
+    def get_body(self, file):
+        """ Read the remaining lines, body part (lyrics) of the song file."""
+        return [ line for line in file ]
+        
